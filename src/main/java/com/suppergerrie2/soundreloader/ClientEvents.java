ffa54fd52c06f69f7d7dd4a938aacb7dc2e1abc7
suppergerrie2 <gerjan2001@gmail.com>
2020-07-11 15:47:31 +0200

    - Holding the button no longer spams reload

--- src/main/java/com/suppergerrie2/soundreloader/ClientEvents.java
+++ src/main/java/com/suppergerrie2/soundreloader/ClientEvents.java
@@ -1,9 +1,14 @@
 package com.suppergerrie2.soundreloader;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.SoundEngine;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.client.util.InputMappings;
+import net.minecraft.util.Util;
+import net.minecraft.util.text.Color;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.client.settings.KeyModifier;
@@ -15,6 +20,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
 
+
 public class ClientEvents {
     public static final KeyBinding reloadSoundsKey = new KeyBinding(SoundReloader.MOD_ID + ".key.reload_sounds",
                                                                     KeyConflictContext.UNIVERSAL,
@@ -23,6 +29,7 @@ public class ClientEvents {
                                                                     GLFW.GLFW_KEY_G,
                                                                     "key.categories." + SoundReloader.MOD_ID);
     private static final Logger LOGGER = LogManager.getLogger();
+    boolean wasPressed = false;
 
     public ClientEvents() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetupEvent);
@@ -34,12 +41,22 @@ public class ClientEvents {
     }
 
     public void keyEvent(InputEvent.KeyInputEvent event) {
-        if (reloadSoundsKey.isPressed() || reloadSoundsKey.isKeyDown()) {
-            SoundEngine engine = Minecraft.getInstance().getSoundHandler().sndManager;
+        if (reloadSoundsKey.isPressed()) {
+            if(!wasPressed) {
+                LOGGER.info("Reloading sound engine...");
+                Minecraft.getInstance().getSoundHandler().sndManager.reload();
+                ClientPlayerEntity player = Minecraft.getInstance().player;
+                if (player != null) {
+                    player.sendMessage(new StringTextComponent("[SoundReloader] Sound engine reloaded")
+                                               .func_230530_a_(Style.field_240709_b_.func_240718_a_(
+                                                       Color.func_240744_a_(TextFormatting.YELLOW))),
+                                       Util.field_240973_b_);
+                }
+            }
 
-            LOGGER.info("RELOADING SOUND ENGINE");
-
-            engine.reload();
+            wasPressed = true;
+        } else {
+            wasPressed = false;
         }
     }
 
