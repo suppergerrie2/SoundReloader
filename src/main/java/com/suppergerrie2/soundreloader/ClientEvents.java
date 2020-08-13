5422ab183f71793f5687a22c441122dcb8290d1a
suppergerrie2 <gerjan2001@gmail.com>
2020-08-13 12:20:21 +0200

    - Update to 1.16.2 - Fix missing translations

--- src/main/java/com/suppergerrie2/soundreloader/ClientEvents.java
+++ src/main/java/com/suppergerrie2/soundreloader/ClientEvents.java
@@ -4,11 +4,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.client.util.InputMappings;
-import net.minecraft.util.Util;
 import net.minecraft.util.text.Color;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.client.settings.KeyModifier;
@@ -47,10 +46,11 @@ public class ClientEvents {
                 Minecraft.getInstance().getSoundHandler().sndManager.reload();
                 ClientPlayerEntity player = Minecraft.getInstance().player;
                 if (player != null) {
-                    player.sendMessage(new StringTextComponent("[SoundReloader] Sound engine reloaded")
-                                               .func_230530_a_(Style.field_240709_b_.func_240718_a_(
+
+                    player.sendStatusMessage(new TranslationTextComponent("ssoundreloader.message.reload_done")
+                                               .setStyle(Style.EMPTY.setColor(
                                                        Color.func_240744_a_(TextFormatting.YELLOW))),
-                                       Util.field_240973_b_);
+                                       false);
                 }
             }
 
